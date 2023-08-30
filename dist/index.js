e457d7bbaa6a01f085e29c00178c98d2264f29d5
Source Coty <coty.massmann@gmail.com>
2023-08-30 21:00:07 +0700

    Complete essential DOM, JS, HMTL, and CSS

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
+let projectTitle = "";
+let taskTitle = "";
+let projectIndex = "";
+let taskIndex = "";
+let taskDiv = "";
+
 const projects = [
     {title: "Test", tasks: [{title: "Task Test", date: "2023-08-20", priority: "high"}]},
     {title: "Test 2", tasks: [{title: "Task Test 2", date: "2023-08-20", priority: "medium"}]},
@@ -6,21 +12,9 @@ const projects = [
 
 
 
-let projectTitle = "";
-let taskIndex = "";
-
-
 
 const projectForm = document.getElementById('projectForm');
 projectForm.addEventListener('submit', appendProjectData);
-
-
-
-
-
-// const projectDisplay = document.getElementById("projectDisplay")
-// projectDisplay.addEventListener("click", displayTasks)
-// projectDisplay.addEventListener("click", getProjectTitle)
 
 
 
@@ -67,7 +61,8 @@ function getProjectIndex(projectTitle) {
     for (obj of projects) {
         if (obj.title === projectTitle) {
             projectIndex = projects.indexOf(obj)
-            deleteProject(projectIndex)
+            // deleteProject(projectIndex)
+            return projectIndex
         }
     }
 }
@@ -78,20 +73,22 @@ function deleteProject(index) {
 
 function processProjectDelete(event) {
     projectTitle = event.target.parentNode.previousElementSibling.querySelector(".projectTitle").innerText
-    console.log(projectTitle)
+    projectIndex = ""
 
     getProjectIndex(projectTitle)
-
-
+    deleteProject(projectIndex)
     displayProject()
     inputNewProjects()
     inputProjectDelete()
 }
 
 
+
+
+
+
 function getProjectTitle(event) {
     projectTitle = event.target.innerText
-
     return projectTitle;
 }
 
@@ -101,17 +98,18 @@ function getProjectTitle(event) {
 
 function clearProjectTitle() {
     projectTitle = "";
-
     return projectTitle;
 }
 
 
+function coupleTasksAndDisplay(event) {
+    getProjectTitle(event)
+    displayTasks()
+}
 
 
 
-
-function displayTasks(event) {
-    const projectTitle = event.target.innerText
+function displayTasks() {
    
     taskDisplay.innerHTML = '';
 
@@ -159,6 +157,8 @@ function displayTasks(event) {
         }
     }
     } 
+    inputTaskDelete()
+    editForm()
 }
 
 
@@ -249,7 +249,7 @@ function createTask (event, task) {
     taskDisplay.setAttribute("id", "taskDisplay");
     newTask.className = "task"
     taskTitle.className = "taskTitle";
-    taskDate.className = "taskDAte";
+    taskDate.className = "taskDate";
     taskIcons.className = "icons"
     taskDeleteButton.className="deleteBtn";
     taskDeleteButton.setAttribute("id", "taskDelete");
@@ -281,20 +281,52 @@ function createTask (event, task) {
  
 function appendTask (event) {
     inputTaskData(event);
+    inputTaskDelete()
+    editForm()
 };
 
 
 
 
 
-function getTaskIndex(title) {
-    for (let tasks of projects) {
-        for (let task of tasks) {
-            if (task.title == title) {
-                console.log(projects.indexOf(tasks))
+function deleteTask(projectIndex, taskIndex) {
+    projects[projectIndex].tasks.splice(taskIndex, 1)
+}
+
+
+
+
+
+function getTaskIndex(projectTitle, taskTitle) {
+    for (obj of projects) {
+        if (obj.title === projectTitle) {
+            projectIndex = projects.indexOf(obj);
+            // console.log(projectIndex)
+            for (obj of projects[projectIndex].tasks) {
+                if (obj.title === taskTitle) {
+                    console.log(taskTitle)
+                    taskIndex = projects[projectIndex].tasks.indexOf(obj)
+                }
             }
         }
     }
+
+}
+
+
+
+
+
+
+function processTaskDelete(event) {
+    taskTitle = event.target.parentNode.parentNode.querySelector(".taskTitle").innerText
+
+    getTaskIndex(projectTitle, taskTitle)
+    deleteTask(projectIndex, taskIndex)
+    displayProject()
+    displayTasks()
+    inputNewProjects()
+    inputProjectDelete()
 }
 
 
@@ -325,19 +357,146 @@ function respondToClick(event) {
     // getProjectIndex(projectTitle);
 }
 
-// const projectContainer = document.querySelectorAll(".project")
-// projectContainer.forEach(el => el.addEventListener("click", respondToProjectClick))
+function getTaskDiv(event) {
+    taskDiv = event.target.parentNode.parentNode
+
+}
+
+function createEditForm() {
+
+    taskDiv.innerHTML = ""
+    taskDiv.style.backgroundColor = "white";
+
+                 const editForm = document.createElement("FORM");
+                 const title = document.createElement("INPUT");
+                 const titleLabel = document.createElement("LABEL")
+                 const date = document.createElement('INPUT');
+                 const dateLabel = document.createElement("LABEL")
+                 const priority = document.createElement("select");
+                 const priorityLabel = document.createElement("LABEL")
+                 const submitButton = document.createElement("INPUT");
+                 const high = document.createElement("option")
+                 const medium = document.createElement("option")
+                 const low = document.createElement("option")
+ 
+     
+                 taskDisplay.setAttribute("id", "taskDisplay");
+                 editForm.className = "editForm"
+                 title.className = "editTitle";
+                 titleLabel.htmlFor = "title";
+                 titleLabel.hidden = true;
+                 date.className = "editDate";
+                 dateLabel.htmlFor = "date";
+                 dateLabel.hidden = true;
+                 priority.className = "editPriority";
+                 priorityLabel.htmlFor = "priority";
+                 priorityLabel.hidden = true;
+                 submitButton.className="submitBtn";
+                 submitButton.setAttribute("type", "submit")
+                 high.value = "high"
+                 medium.value = "medium"
+                 low.value = "low"
+     
+                 title.setAttribute("type", "text")
+                 title.name = "title";
+                 title.placeholder = "Your new title";
+                 date.setAttribute("type", "date")
+                 date.name = "date";
+                 priority.name = "priority";
+                 high.innerText = "high";
+                 medium.innerText = "medium";
+                 low.innerText = "low";
+                 submitButton.innerText = "Submit"
+     
+                 taskDiv.appendChild(editForm);
+                 editForm.appendChild(title);
+                 editForm.appendChild(titleLabel);
+                 editForm.appendChild(date);
+                 editForm.appendChild(dateLabel);
+                 editForm.appendChild(priority);
+                 priority.appendChild(high);
+                 priority.appendChild(medium);
+                 priority.appendChild(low);
+                 editForm.appendChild(priorityLabel)
+                 editForm.appendChild(submitButton);
+}
+
+ function appendEditForm(event) {
+     getTaskDiv(event)
+     createEditForm()
+     submitEdit()
+ }
+
+
+
+ function inputNewData (event) {
+    event.preventDefault();
+
+    console.log(event.target)
+    
+    const formData = new FormData(event.target);
+    const task = {};
+    
+    formData.forEach((value, key) => (task[key] = value));
+    
+    for (obj of projects[projectIndex].tasks) {
+        if (obj.title === taskTitle) {
+            projects[projectIndex].tasks.splice(taskIndex, 1, task)
+        }
+    }
+    createTask(event, task)
+ }
+
+ function getTaskTitle(event) {
+    taskTitle = event.target.parentNode.parentNode.querySelector(".taskTitle").innerText
+    // console.log(taskTitle)
+ }
+
+
+ function appendTaskEdit(event) {
+
+    getTaskIndex(projectTitle, taskTitle)
+    inputNewData(event);
+    inputTaskDelete()
+    editForm()
+    displayTasks()
+    
+};
+
+
+
+
+
+
+function editForm() {
+    const editTaskButton = document.querySelectorAll('.edit')
+    editTaskButton.forEach(el => el.addEventListener("click", getTaskTitle))
+    editTaskButton.forEach(el => el.addEventListener("click", appendEditForm))
+}
+
+function submitEdit() {
+    const editForm = document.querySelectorAll(".editForm")
+    editForm.forEach(el => el.addEventListener("submit",appendTaskEdit))
+}
+
+
+function inputTaskDelete() {
+    const deleteTaskButton = document.querySelectorAll('.deleteBtn')
+    deleteTaskButton.forEach(el => el.addEventListener('click', processTaskDelete))
+}
+inputTaskDelete()
+
 
 function inputProjectDelete() {
-const deleteTaskButton = document.querySelectorAll('.projectDelete')
-deleteTaskButton.forEach(el => el.addEventListener('click', processProjectDelete))
+    const deleteProjectButton = document.querySelectorAll('.projectDelete')
+    deleteProjectButton.forEach(el => el.addEventListener('click', processProjectDelete))
 }
 inputProjectDelete()
 
 function inputNewProjects() {
     const projectSelector = document.querySelectorAll(".project");
-    projectSelector.forEach(el => el.addEventListener("click", displayTasks))
+    projectSelector.forEach(el => el.addEventListener("click", coupleTasksAndDisplay))
     projectSelector.forEach(el => el.addEventListener("click", getProjectTitle))
 }
-
 inputNewProjects()
+
